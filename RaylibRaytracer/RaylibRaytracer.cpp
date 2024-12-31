b386ce3dd21245a54d13f3dc23682b9a86a6d9dd
x32dev <120539187+FlexTechDev@users.noreply.github.com>
2024-12-31 12:46:31 -0600

    readm uploads

--- RaylibRaytracer/RaylibRaytracer.cpp
+++ RaylibRaytracer/RaylibRaytracer.cpp
@@ -25,35 +25,45 @@ int main()
 
 	DisableCursor();
 
-	TracingEngine::Initialize(Vector2(2048, 1024), 5, 10, 0.001f);
+	TracingEngine::Initialize(Vector2(2048, 1024), 7, 10, 0.001f);
 
 	TracingEngine::skyMaterial = SkyMaterial{ WHITE, SKYBLUE, BROWN, WHITE, Vector3(-0.5f, -1, -0.5f), 1, 0.5 };
 
 	RaytracingMaterial red = { Vector4(1,1,1,1), Vector4(1,0,0,10), Vector4(0,0,0,0) };
-	RaytracingMaterial red2 = { Vector4(1,0.5f,0.5f,0), Vector4(0,0,0,0), Vector4(0,0,0,0) };
+	RaytracingMaterial red2 = { Vector4(1,0.6f,0.6f,0), Vector4(0,0,0,0), Vector4(0,0,0,0) };
 	RaytracingMaterial green = { Vector4(1,1,1,1), Vector4(0,0,1,10), Vector4(0,0,0,0) };
 	RaytracingMaterial blue = { Vector4(1,1,1,1), Vector4(0,1,0,10), Vector4(0,0,0,0) };
 	RaytracingMaterial white = { Vector4(1,1,1,1), Vector4(0,0,0,0), Vector4(0,0,0,0) };
 	RaytracingMaterial grey = { Vector4(0.5f,0.5f,0.5f,1), Vector4(0,0,0,0), Vector4(0,0,0,0) };
-	RaytracingMaterial light = { Vector4(1,0.8f,0.7f,1), Vector4(1,1,1,0.5f), Vector4(0,0,0,0) };
+	RaytracingMaterial light = { Vector4(1,0.8f,0.7f,1), Vector4(1,1,1,1.2f), Vector4(0,0,0,0) };
 	RaytracingMaterial metal = { Vector4(1,1,1,1), Vector4(0,0,0,0), Vector4(0,1,0,0) };
 
-	TracingEngine::spheres.push_back({ Vector3(0, 50, 100), 50, light });
-	TracingEngine::spheres.push_back({ Vector3(3, 1, 0), 1, white });
-
-	Model dragon = LoadModel("resources/meshes/stanford_dragon.obj");
-	TracingEngine::UploadRaylibModel(dragon, metal, false, 14);
-
-	Model monkey = LoadModel("resources/meshes/monkey.obj");
-	monkey.transform = MatrixTranslate(-3, 1, 0);
-	TracingEngine::UploadRaylibModel(monkey, red2, false, 8);
+	Model dragon = LoadModel("resources/meshes/monkey.obj");
+	dragon.transform = MatrixTranslate(0, 1, -1);
+	TracingEngine::UploadRaylibModel(dragon, red2, false, 7);
 
 	Model floor = LoadModelFromMesh(GenMeshPlane(50, 50, 1, 1));
 	TracingEngine::UploadRaylibModel(floor, white, true, 0);
 
 	Model wall = LoadModelFromMesh(GenMeshPlane(50, 50, 1, 1));
-	wall.transform = MatrixRotateX(PI / 2) * MatrixTranslate(0, 0, -7);
+	wall.transform = MatrixRotateX(PI / 2) * MatrixTranslate(0, 0, -2);
 	TracingEngine::UploadRaylibModel(wall, white, true, 0);
+
+	Model ceiling = LoadModelFromMesh(GenMeshPlane(50, 50, 1, 1));
+	ceiling.transform = MatrixRotateX(PI) * MatrixTranslate(0, 3, 0);
+	TracingEngine::UploadRaylibModel(ceiling, white, true, 0);
+
+	Model lighting = LoadModelFromMesh(GenMeshCube(2, 1, 2));
+	lighting.transform = MatrixTranslate(0, 3, 0);
+	TracingEngine::UploadRaylibModel(lighting, light, true, 0);
+
+	Model left = LoadModelFromMesh(GenMeshPlane(50, 50, 1, 1));
+	left.transform = MatrixRotateZ(-PI / 2) * MatrixTranslate(-2, 0, 0);
+	TracingEngine::UploadRaylibModel(left, white, true, 0);
+
+	Model right = LoadModelFromMesh(GenMeshPlane(50, 50, 1, 1));
+	right.transform = MatrixRotateZ(PI / 2) * MatrixTranslate(2, 0, 0);
+	TracingEngine::UploadRaylibModel(right, white, true, 0);
 
 	TracingEngine::UploadStaticData();
 
@@ -73,10 +83,13 @@ int main()
 		deltaTime += GetFrameTime();
 	}
 	
-	UnloadModel(monkey);
 	UnloadModel(dragon);
 	UnloadModel(floor);
 	UnloadModel(wall);
+	UnloadModel(left);
+	UnloadModel(right);
+	UnloadModel(ceiling);
+	UnloadModel(lighting);
 
 	TracingEngine::Unload();
 
